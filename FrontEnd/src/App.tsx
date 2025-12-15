16874eda86f7ca4155d7d1c288abac982bd3c000
soul <mo7amed.talb@gmail.com>
2025-12-15 20:22:56 +0100

    recreat  the deleted files (App.tsx & main.tsx)

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import { SetUpTable } from '../ReportTable/report-table.tsx'
+import ReportPage from './Pages/ReportsDetails/ReportDetails';
+// import ReportPage from './all.tsx';
+export function App()
+{
+    return (
+    //    <SetUpTable />
+        <ReportPage />
+    )
+}
