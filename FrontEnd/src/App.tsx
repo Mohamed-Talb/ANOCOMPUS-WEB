5ffea3fb6eaf737c2546d65d2a1db1c61afff23e
khalid <khalidelbahry33@gmail.com>
2025-12-28 15:30:47 +0100

    announcement page

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -4,25 +4,19 @@ import Dashboard from './Pages/Dashboard/Dashboard'
 import Archive from './Pages/Archive/Archive'
 import Reports from './Pages/Reports/Reports'
 import ReportDetails from './Pages/ReportsDetails/ReportDetails'
-import AnnouncementForm from './Pages/Annoncements/AnnouncementForm'
-
-// export function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Layout />}>
-//         <Route index element={<Dashboard />} />
-//         <Route path="dashboard" element={<Dashboard />} />
-//         <Route path="reports" element={<Reports />} />
-//         <Route path="reports/:id" element={<ReportDetails />} />
-//         <Route path="archive" element={<Archive />} />
-//       </Route>
-//     </Routes>
-//   )
-// }
+import AnnouncementsPage from './Pages/Annoncements/AnnouncementsPage'
 
 export function App() {
   return (
-    // <AnnouncementForm />
-    <Reports />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="reports" element={<Reports />} />
+        <Route path="reports/:id" element={<ReportDetails />} />
+        <Route path="archive" element={<Archive />} />
+        <Route path="announcements" element={<AnnouncementsPage />} />
+      </Route>
+    </Routes>
   )
 }
